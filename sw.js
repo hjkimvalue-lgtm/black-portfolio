25932ab8393b448e4c65912d7baa6d0fb3c6be93
black <hjkimvalue@gmail.com>
2026-03-17 14:20:08 +0900

    first commit

---
+++ sw.js
@@ -0,0 +1,38 @@
+// 블랙 가상포트폴리오 Service Worker
+const CACHE_NAME = 'black-portfolio-v1';
+const urlsToCache = [
+  '/portfolio_app.html',
+  '/manifest.json'
+];
+
+// 설치 시 캐시
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// 활성화 시 이전 캐시 삭제
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// 네트워크 우선 전략 (항상 최신 데이터 가져오기)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
